538e805a721e1da00b6b7f97b143b577625cca50
LilGii <angelhernandezfrancisco9@gmail.com>
2026-03-09 12:40:17 -0600

    Ver 3.0

---
+++ env.js
@@ -0,0 +1,13 @@
+// env.js — SOLO PARA DESARROLLO LOCAL
+// Este archivo es reemplazado automáticamente en Vercel por vercel.json
+// ⚠️  AGREGA env.js A TU .gitignore — nunca lo subas con credenciales reales
+
+window.__ENV__ = {
+  FIREBASE_API_KEY:            "AIzaSyAIgHSI_owi9QZJSg6aRENfj1QVCEtdSN0",
+  FIREBASE_AUTH_DOMAIN:        "pizzeria-b6f97.firebaseapp.com",
+  FIREBASE_PROJECT_ID:         "pizzeria-b6f97",
+  FIREBASE_STORAGE_BUCKET:     "pizzeria-b6f97.firebasestorage.app",
+  FIREBASE_MESSAGING_SENDER_ID:"202370819887",
+  FIREBASE_APP_ID:             "1:202370819887:web:231e9e0c782ed58517a3da",
+  FIREBASE_MEASUREMENT_ID:     "G-WRDEFZQP67",
+};
